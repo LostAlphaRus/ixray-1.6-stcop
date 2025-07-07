5cf9d492e0ff1a97ac1733e1daaa5d4b8734c699
Shtrecker <gogels235@gmail.com>
2025-07-07 14:07:46 +0300

    Add `block_reload` parametr

--- src/xrGame/Weapon.cpp
+++ src/xrGame/Weapon.cpp
@@ -630,6 +630,8 @@ void CWeapon::Load		(LPCSTR section)
 
 	m_bRestGlSil = READ_IF_EXISTS(pSettings, r_bool, section, "restricted_gl_and_sil", false);
 
+	m_bBlockReload = READ_IF_EXISTS(pSettings, r_bool, section, "block_reload", false);
+
 	if (pSettings->line_exist(hud_sect, "shell_params_section"))
 	{
 		SAmmoBonesParams* bone_params = new SAmmoBonesParams(undefined_ammo_type);
@@ -1577,6 +1579,11 @@ bool CWeapon::SwitchAmmoType( u32 flags )
 	if ( !(flags & CMD_START) )
 		return false;
 
+	if (m_bBlockReload && GetState() != eIdle)
+	{
+		return false;
+	}
+
 	if (IsMisfire())
 	{
 		return false;
@@ -1587,7 +1594,7 @@ bool CWeapon::SwitchAmmoType( u32 flags )
 
 	if (bReloadKeyPressed || bAmmotypeKeyPressed)
 		return false;
-	
+
 	bAmmotypeKeyPressed = true;
 
 	if (ParentIsActor() && Actor()->GetDetector() && Actor()->GetDetector()->GetState() != CCustomDetector::eIdle)
--- src/xrGame/Weapon.h
+++ src/xrGame/Weapon.h
@@ -250,6 +250,7 @@ public:
 	bool m_bIsAimStarted = false;
 	bool m_bRestGlSil;
 	bool m_bTacticalTorchStatus = false;
+	bool m_bBlockReload;
 
 	shared_str hud_silencer;
 	shared_str hud_scope;
