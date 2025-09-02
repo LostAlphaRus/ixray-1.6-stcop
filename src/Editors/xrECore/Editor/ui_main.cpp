1c67cf21844c53c297194706681cd8845fe017b2
ForserX <sv3nk@yandex.ru>
2025-09-02 19:55:43 +0300

    Fix

--- src/Editors/xrECore/Editor/ui_main.cpp
+++ src/Editors/xrECore/Editor/ui_main.cpp
@@ -539,9 +539,7 @@ void TUI::Redraw()
 				g_bRendering = FALSE;
 				// 
 				ID3D11RenderTargetView* RTV = RSwapchainTarget;
-				 //  Draw(); 
-				   // end draw
-				xrCriticalSectionGuard guard(EDevice->Dx11Guard);
+				EDevice->Dx11Guard.Enter();
 				UI->BeginFrame();
 
 				Draw();
@@ -550,6 +548,7 @@ void TUI::Redraw()
 
 				RContext->OMSetRenderTargets(1, &RTV, 0);
 				UI->EndFrame();
+				EDevice->Dx11Guard.Leave();
 				EDevice->End();
 				UI->MDIUpdate();
 			}
